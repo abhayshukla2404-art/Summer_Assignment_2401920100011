8947af64ace12d5094c1f4515c5d3fd1523adda5
Abhay Shukla <rishu.mahi71@gmail.com>
2026-06-04 20:42:33 +0530

    Added solutions

---
+++ Week_1/Day_4/ques2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        if (m * n != r * c) {
+            return mat;
+        }
+
+        vector<vector<int>> res(r, vector<int>(c));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int idx = i * n + j;
+
+                res[idx / c][idx % c] = mat[i][j];
+            }
+        }
+
+        return res;
+    }
+};
