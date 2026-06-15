e42316be465772376f4a613fcb345901078b6d46
Abhay Shukla <rishu.mahi71@gmail.com>
2026-06-15 23:34:09 +0530

    initial commit

---
+++ Week_3/Day_1/ques2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+            ListNode* nextNode = curr->next; // save next node
+            curr->next = prev;               // reverse link
+            prev = curr;                     // move prev
+            curr = nextNode;                 // move curr
+        }
+
+        return prev;
+    }
+};
